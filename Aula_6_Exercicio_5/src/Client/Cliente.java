2ee40aca584bd7f0d7e5c40b1c2787001b872dde
ecunha <ecunha@EJCJ1034.EJCJ.info>
2019-04-11 16:41:30 -0300

    V.1.0.1

---
+++ Aula_6_Exercicio_5/src/Client/Cliente.java
@@ -0,0 +1,123 @@
+package Client;
+
+ 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.Scanner;
+
+public class Cliente {
+
+	/**
+	 * 5. Escreva um programa distribuído onde um processo do tipo A (servidor) é
+	 * lançado e para esse é escolhido um número (passado como parâmetro). Processos
+	 * do tipo B (clientes) devem ser lançados e escolherem seus números (passados
+	 * como parâmetro). Os clientes devem enviar seus números ao servidor, que deve
+	 * responder “Acertou” aos clientes que adivinharem o número escolhido e “Errou”
+	 * aos outros clientes.
+	 * 
+	 * @author Evandro Rocha da Cunha - Miguel Luciano Benites da Silva
+	 *
+	 */
+
+	public static void main(String[] args) {
+
+	}
+
+	private void enviarMensagem() throws InterruptedException, SocketException {
+
+		System.out.println("Informe o número escolhido: ");
+		Scanner sc = new Scanner(System.in);
+		// LENDO RESPOSTA DO CLIENTE
+		String numero = sc.nextLine();
+		System.out.println("Resposta armazenada...");
+		Thread.sleep(3000);
+
+		try {
+			System.out.println("Criando Socket para envio de mensagem...");
+			Thread.sleep(3000);
+			// CRIANDO O SOCKET PARA A CONEXÃO COM O SERVER
+			DatagramSocket socket = new DatagramSocket(30000);
+			// BUFFER PARA ARMAZENAMENTO DA RESPOSTA DO USUÁRIO EM BYTES
+			System.out.println("Socket criado com sucesso!");
+			Thread.sleep(3000);
+			System.out.println("Criando buffer de dados para envio...");
+			Thread.sleep(3000);
+			byte[] dadosEnvio = new byte[1024];
+			System.out.println("Convertendo resposta do client em bytes");
+			Thread.sleep(3000);
+			// TRANSFORAMANDO EM BYTES A RESPOSTA DO CLIENTE
+			dadosEnvio = numero.getBytes();
+			System.out.println("Conversão realizada com sucesso!");
+			Thread.sleep(3000);
+			System.out.println("Informando IP e PORTA do server para envio...");
+			Thread.sleep(3000);
+			// INFORMANDO IP DE ENVIO DO SERVER
+			InetAddress serv_ip = InetAddress.getByName("localhost");
+			// INFORMANDO A PORTA DE ENVIO PARA O SERVER
+			int serv_port = 30000;
+			System.out.println("Criando pacote para envio...");
+			Thread.sleep(3000);
+
+			// MONTANDO O PACOTE DE ENVIO
+			DatagramPacket PacoteEnvio = new DatagramPacket(dadosEnvio, dadosEnvio.length, serv_ip, serv_port);
+			System.out.println("pacote criado com sucesso!");
+			Thread.sleep(3000);
+			// ENVIANDO PACOTE
+			System.out.println("Enviando pacote...");
+			Thread.sleep(3000);
+			socket.send(PacoteEnvio);
+			System.out.println("Pacote enviado com sucesso!");
+			Thread.sleep(3000);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Aguardando resposta do server...");
+		Thread.sleep(3000);
+		System.out.println("Criando Socket para recebimento de mensagem...");
+		Thread.sleep(3000);
+		// CRIANDO O SOCKET PARA A CONEXÃO COM O SERVER NA PORTA 30000
+		DatagramSocket socket = new DatagramSocket(30000);
+		// RECEBENDO RESPOSTA DO SERVER
+		System.out.println("Criando buffer para resposta...");
+		Thread.sleep(3000);
+		byte[] respostaServer = new byte[1024];
+		System.out.println("Buffer criado!");
+		Thread.sleep(3000);
+		try {
+			System.out.println("Criando pacote para recebimento...");
+			Thread.sleep(3000);
+			DatagramPacket pacoteRecebido = new DatagramPacket(respostaServer, respostaServer.length);
+			System.out.println("Pacote criado com sucesso!");
+			Thread.sleep(3000);
+
+			socket.setSoTimeout(1000);
+			System.out.println("Recebendo pacote...");
+			Thread.sleep(3000);
+
+			socket.receive(pacoteRecebido);
+			System.out.println("Pacote transferido com sucesso!");
+			Thread.sleep(3000);
+
+			// mostra a resposta
+			// CONVERTENDO EM INTEIRO A A RESPOSTA
+			String respostaRecebida = ByteBuffer.wrap(respostaServer).toString();
+			System.out.println("Resposta" + " = " + respostaRecebida);
+		} catch (IOException e) {
+			System.out.println("Servidor offline.");
+			// fecha o socket
+			System.out.println("Fechando conexão...");
+			Thread.sleep(3000);
+			socket.close();
+			System.out.println("Conexão finalizada!");
+		}
+
+	}
+
+}
---
+++ Aula_6_Exercicio_5/src/Server/Servidor.java
@@ -0,0 +1,117 @@
+
+package Server;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.nio.ByteBuffer;
+
+/**
+ * 5. Escreva um programa distribuído onde um processo do tipo A (servidor) é
+ * lançado e para esse é escolhido um número (passado como parâmetro). Processos
+ * do tipo B (clientes) devem ser lançados e escolherem seus números (passados
+ * como parâmetro). Os clientes devem enviar seus números ao servidor, que deve
+ * responder “Acertou” aos clientes que adivinharem o número escolhido e “Errou”
+ * aos outros clientes.
+ * 
+ * @author Evandro Rocha da Cunha - Miguel Luciano Benites da Silva
+ *
+ */
+
+public class Servidor {
+
+	public static void main(String[] args) {
+		Servidor server = new Servidor();
+		try {
+			server.processarMensagem();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void processarMensagem() throws SocketException, InterruptedException {
+
+		// SORTEIA O NÚMERO de 1 a 100
+		int numeroSorteado = (int) (Math.random() * 100);
+		System.out.println("Número sorteado =  " + numeroSorteado);
+		Thread.sleep(3000);
+		byte[] buff_out = new byte[1024];
+		byte[] buff_in = new byte[1024];
+
+		System.out.println("Criando socket ...");
+		Thread.sleep(3000);
+		// SOCKET PARA CONEXÃO ENTRE CLIENT E SERVER
+		DatagramSocket socket = new DatagramSocket(30000);
+		System.out.println("Criando pacote para armazenar mensagem recebida...");
+		Thread.sleep(3000);
+		// CRIANDO ESTRUTURA DE ARMAZENAMENTO DE PACOTE
+		DatagramPacket input = new DatagramPacket(buff_in, buff_in.length);
+		System.out.println("Aguardando mensagem...");
+		Thread.sleep(3000);
+
+		while (true) {
+			try {
+
+				// RECEBENDO PACOTE
+				socket.receive(input);
+				System.out.println("Recebendo mensagem...");
+				Thread.sleep(3000);
+				// TRADUZINDO MENSAGEM RECEBIDA
+				String numero = new String(input.getData(), 0, input.getLength());
+				// TRANSFORMANDO EM INTEIRO
+				int numeroCliente = Integer.parseInt(numero);
+				// PEGANDO IP PARA RESPOSTA
+				InetAddress resp_ip = input.getAddress();
+				// PEGANDO PORTA PARA RESPOSTA
+				int resp_port = input.getPort();
+				System.out.println("Conectado a  " + resp_ip + "na porta  " + resp_port);
+				Thread.sleep(3000);
+				System.out.println("Número informado pelo cliente:  " + numeroCliente);
+				Thread.sleep(3000);
+
+				// VARIÁVEL QUE ARMAZENA A MENSAGEM DE RESPOSTA AO CLIENTE
+				String mensagemResposta = "";
+
+				// FILTRANDO A RESPOSTA
+				if (numeroCliente == numeroSorteado) {
+					mensagemResposta = "ACERTOU";
+
+				} else {
+					mensagemResposta = "ERROU";
+
+				}
+				System.out.println("Cliente  " + mensagemResposta + "!!!");
+				Thread.sleep(3000);
+
+				// CONVERTENDO EM BYTES A MENSAGEM
+				byte[] resposta = mensagemResposta.getBytes();
+				// GRAVANDO NO BUFFER A RESPOSTA
+				ByteBuffer.wrap(buff_out).put(resposta);
+				// MONTANDO O PACOTE COM A RESPOSTA
+				System.out.println("Montando pacote para a resposta...");
+				Thread.sleep(3000);
+				DatagramPacket output = new DatagramPacket(buff_out, buff_out.length, resp_ip, resp_port);
+				// ENVIANDO A RESPOSTA
+				socket.send(output);
+				System.out.println("Enviando a mensagem...");
+				Thread.sleep(3000);
+			} catch (IOException e) {
+				e.printStackTrace();
+				break;
+			}
+		}
+		System.out.println("Mensagem enviada!!!");
+		Thread.sleep(3000);
+		// FECHANDO A CONEXÃO
+		socket.close();
+		System.out.println("Fechando conexão!");
+		Thread.sleep(3000);
+	}
+}
